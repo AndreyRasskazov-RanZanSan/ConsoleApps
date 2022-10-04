30b9afd5ca748d4a6d31e8b79d886cd0b58badac
Rasskazov Andrey <ranzansan@gmail.com>
2022-10-04 16:24:44 +0300

    update

---
+++ PeaceForAllWorld/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeaceForAllWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Peace for all world.");
+            Console.ReadKey();
+        }
+    }
+}
