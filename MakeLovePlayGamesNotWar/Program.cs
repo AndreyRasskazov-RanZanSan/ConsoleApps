38b830c23ca06b434014e5bc2629aa0fc5813691
Rasskazov Andrey <ranzansan@gmail.com>
2022-10-04 19:18:04 +0300

    Make Love, Play Games, Not War!

---
+++ MakeLovePlayGamesNotWar/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MakeLovePlayGamesNotWar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Make Love, Play Games, Not War!");
+            Console.ReadKey();
+        }
+    }
+}
